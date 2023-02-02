4eff66d1b0051e67576e097bf72cc728d00b6dd0
Ali Shanaakh <github@shal.dev>
2023-02-02 23:28:58 +0000

    Fixes

--- pkg/domain/query/query.go
+++ pkg/domain/query/query.go
@@ -1,12 +1,8 @@
 package query
 
-import (
-	"github.com/opencars/schema"
-)
-
-var (
-	source = schema.Source{
-		Service: "wanted",
-		Version: "1.0",
-	}
-)
+// var (
+// 	source = schema.Source{
+// 		Service: "wanted",
+// 		Version: "1.0",
+// 	}
+// )
